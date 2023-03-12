792b7971bce4bdb6af6453a81bd5aceb46045baa
Pantaiz <pantaiz@yandex.ru>
2023-03-12 22:13:43 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,42 @@ export type TaskType = {
     isDone: boolean
 }
 export type TasksType = TaskType[]
-
+export type todolistType={
+    id:string,
+    title:string,
+    filter:FilterType }
+export type todolistsType=todolistType[]
 export type FilterType = 'all' | 'active' | 'completed'
+const todolistId1 = v1()
+const todolistId2 = v1()
 
 function App() {
-    let [tasks, setTask] = useState<TasksType>([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false}
+    let [todolist,setTodolist ]= useState<todolistsType>([
+        {id: todolistId1, title: 'What to learn', filter: 'active'},
+        {id: todolistId2, title: 'What to buy', filter: 'active'}
     ])
+    let [tasks, setTask] = useState<>(
+        {
+            [todolistId1]: [
+                {id: v1(), title: "HTML&CSS", isDone: true},
+                {id: v1(), title: "JS", isDone: true},
+                {id: v1(), title: "ReactJS", isDone: false}],
+            [todolistId2]: [
+                {id: v1(), title: "HTML&CSS", isDone: true},
+                {id: v1(), title: "JS", isDone: true},
+                {id: v1(), title: "ReactJS", isDone: false}]
+        }
+    )
     const deleteTask = (id: string) => {
         filtredTask = filtredTask.filter(t => t.id !== id)
         setTask(filtredTask)
     }
-    const changeCheked = (idTask:string,newIsDone:boolean) => {
-        setTask(tasks.map(a=>a.id===idTask?{...a,isDone:newIsDone}:a))
+    const changeCheked = (idTask: string, newIsDone: boolean) => {
+        setTask(tasks.map(a => a.id === idTask ? {...a, isDone: newIsDone} : a))
     }
-    const addTask = (title:string) => {
-    const newTask:TaskType={id: v1(), title: title, isDone: false}
-        setTask([newTask,...tasks])
+    const addTask = (title: string) => {
+        const newTask: TaskType = {id: v1(), title: title, isDone: false}
+        setTask([newTask, ...tasks])
 
     }
     const [filter, setFilter] = useState<FilterType>('all')
@@ -42,7 +59,8 @@ function App() {
     return (
         <>
 
-            <TodoList filterValue={filter} changeCheked={changeCheked} addTask={addTask} task={filtredTask} filterHandler={setFilter} deleteTask={deleteTask}/>
+            <TodoList filterValue={filter} changeCheked={changeCheked} addTask={addTask} task={filtredTask}
+                      filterHandler={setFilter} deleteTask={deleteTask}/>
         </>
 
     )
